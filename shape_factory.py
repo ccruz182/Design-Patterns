2900f253cfbdd404923cdd83b582acd8e4e94302
César Cruz A <cesar.cruz182@gmail.com>
2020-12-28 12:57:28 -0600

    Factory pattern, example

---
+++ shape_factory.py
@@ -0,0 +1,19 @@
+from figures import Circle, Rectangle, Square
+
+class ShapeFactory:
+    def create_shape(self, name):
+        if name == 'circle':
+            radius = input("Enter the radius of the circle: ")
+            return Circle(float(radius))
+
+        elif name == 'rectangle':
+            height = input("Enter the height of the rectangle: ")
+            width = input("Enter the width of the rectangle: ")
+            return Rectangle(int(height), int(width))
+
+        elif name == 'square':
+            width = input("Enter the width of the square: ")
+            return Square(int(width))
+
+        else:
+            raise ValueError('Figure not configured')
